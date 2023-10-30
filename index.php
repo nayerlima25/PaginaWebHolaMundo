670e3fcbf09f275a7f4efc57173105541722b664
NayerliMJ <147526607+nayerlima25@users.noreply.github.com>
2023-10-30 17:30:59 -0500

    Agregando todos los archivos al commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido a PHP</h1>
+  
+    <img src="img/paisajes-naturales-26.jpeg" alt="" width=300px >
+    <?php
+
+    echo"HOLA NAYE"
+
+    ?>
+
+
+
+
+</body>
+</html>
+
+
+<?php
+
+
+echo "BIENVENIDO"
+
+?>
+<label > INGRESE NOMBRE : </label>
+
+<input type="text" name="txtNom">
+<label><?php echo"DESARROLLO WEB "; ?></label>
---
+++ suma.php
@@ -0,0 +1,8 @@
+<?php
+$num1=10;
+
+$num2 =7;
+
+$c=$num1+num2;
+echo "Resultado de ".$num1."+".$num2."="$c;
+?>
